40d0883edd5b16d293f917997628ce98b9ff719f
Jihanjie Luo <j92luo@edu.uwaterloo.ca>
2020-09-12 17:33:34 -0700

    set up page routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'pm-root',
+  selector: 'page-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  title = 'Turnip\'s Home Page';
+  showSideNav: boolean = true;
+
+  testClick() {
+    console.log("clicked!");
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    ProjectsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSidenavModule,
+    NgbModule,
+    RouterModule.forRoot([
+      { path: 'home', component: WelcomeComponent },
+      { path: 'projects', component: ProjectsComponent },
+      { path: '', redirectTo: 'home', pathMatch: 'full'},
+      { path: '***', redirectTo: 'projects', pathMatch: 'full'}
+    ])
   ],
   bootstrap: [AppComponent]
 })
