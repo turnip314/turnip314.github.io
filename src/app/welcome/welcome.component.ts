331f54465bad50b89fb7f900006c3c93e2a6c7cb
Jihanjie Luo <j92luo@edu.uwaterloo.ca>
2020-09-17 21:48:33 -0700

    programming langauges section

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -99,7 +99,7 @@ export class WelcomeComponent {
     @HostListener('window:scroll', ['$event']) onScrollEvent($event) {
         console.log("---");
         // console.log(window.pageYOffset);
-        var darkElements = document.getElementsByClassName('dark');
+        var darkElements = document.getElementsByClassName('fade-in');
         for (var i = 0; i < darkElements.length; ++i) {
             var el = <HTMLElement> darkElements[i];
             el.style.opacity = this.getOpacityByRelativeWindowLocation(
