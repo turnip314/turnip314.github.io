e83eab4590f67300c922d56a3944f6afbcdd93b3
Jihanjie Luo <j92luo@edu.uwaterloo.ca>
2020-10-04 14:09:57 -0700

    projects section

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -16,18 +16,51 @@ export class ProjectsComponent {
             demos: [
                 {
                     type: "video",
-                    //src: "https://www.youtube.com/embed/V1KUx4B1k5k",
                     src: this._sanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/V1KUx4B1k5k")
-                },
-                {
+                }, {
                     type: "image",
                     src: "./assets/images/jumpy-turnip-title.png"
-                },
-                {
+                }, {
                     type: "image",
                     src: "./assets/images/jumpy-turnip-selection.png"
+                }, {
+                    type: "image",
+                    src: "./assets/images/jumpy-turnip-code.png"
                 },
-            ]
+            ],
+            description: "Third person shooter game written in C++ and SFML with 4 difficulties, " +
+                    "30 levels, 4 freeplay levels, 8 characters, and 12 upgrades per character. " +
+                    "Credits to Kayla Estacio for the art, including the turnips and background images."
+        },
+        {
+            title: "Minesweeper",
+            view: 0,
+            demos: [
+                {
+                    type: "image",
+                    src: "./assets/images/minesweeper.png"
+                }, {
+                    type: "image",
+                    src: "./assets/images/minesweeper2.png"
+                }, {
+                    type: "image",
+                    src: "./assets/images/minesweeper3.png"
+                },
+            ],
+            description: "A clone of Minesweeper written in Java using the Java Swing library."
+        }, {
+            title: "TurnipTex",
+            view: 0,
+            demos: [
+                {
+                    type: "image",
+                    src: "./assets/images/turnip-tex-code.png"
+                }, {
+                    type: "image",
+                    src: "./assets/images/turnip-tex-img.png"
+                },
+            ],
+            description: "A math text compiler that can produce LaTeX-style math expressions. Work in Progress."
         }
     ]
 
