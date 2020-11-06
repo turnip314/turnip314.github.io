8f77b3a4de2678be9fa5092d49374b7cc22b5a6c
Jihanjie Luo <j92luo@edu.uwaterloo.ca>
2020-11-05 17:10:12 -0800

    add new files

---
+++ src/app/gallery/gallery.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pm-gallery',
+  templateUrl: './gallery.component.html',
+  styleUrls: ['./gallery.component.css']
+})
+export class GalleryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/shared/services/image.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core'
+
+@Injectable()
+export class ImageService {
+    getWelcomeImages() {
+        return [{
+                image: 'assets/images/me-and-vincent.jpg',
+                thumbImage: 'assets/images/me-and-vincent.jpg',
+                alt: 'Card Tower',
+                title: 'Tower of Cards'
+            }, {
+                image: 'assets/images/twin-peaks.jpg', 
+                thumbImage: 'assets/images/twin-peaks.jpg',
+                title: 'Twin Peaks San Francisco',
+                alt: 'Twin Peaks San Francisco'
+            }, {
+                image: 'assets/images/golden-gate-gang-full.jpg',
+                thumbImage: 'assets/images/golden-gate-gang.jpg',
+                title: 'Right Hand Rule',
+                alt: 'Physics Gang' 
+            }, {
+                image: 'assets/images/golden-gate-squat-full.jpg', 
+                thumbImage: 'assets/images/golden-gate-squat.jpg', 
+                title: 'Golden Gate Gang', 
+                alt: 'Golden Gate Gang' 
+            }, {
+                image: 'assets/images/mathcamp-canada-day-full.jpg', 
+                thumbImage: 'assets/images/mathcamp-canada-day.jpg', 
+                title: 'Canada Day at Mathcamp', 
+                alt: 'Canada Day at Mathcamp' 
+            }, {
+                image: 'assets/images/mexico-full.jpg', 
+                thumbImage: 'assets/images/mexico.jpg', 
+                title: 'Family Trip to Mexico', 
+                alt: 'Family Trip to Mexico' 
+            }
+        ];
+    }
+
+    getGalleryImages() {
+        return [];
+    }
+}
