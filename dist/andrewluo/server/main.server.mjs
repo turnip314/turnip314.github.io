6cf20c653df7a976641beb82eea477dd5bd2bfc2
Andrew Luo <j92luo@uwaterloo.ca>
2026-03-20 21:22:40 -0400

    Delete dist directory

--- dist/andrewluo/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{b as r,g as e,h as t,j as o,k as p,l as m}from"./chunk-IUDN4HW2.mjs";import"./chunk-S6KH3LOX.mjs";import a from"./angular-app-manifest.mjs";e(a);export{m as default,p as \u0275destroyAngularServerApp,t as \u0275extractRoutesAndCreateRouteTree,o as \u0275getOrCreateAngularServerApp,r as \u0275resetCompiledComponents};
