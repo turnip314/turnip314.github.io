354d78517e7b4277af69f4debeb0e47079cdc5fd
Andrew Luo <j92luo@uwaterloo.ca>
2026-04-02 15:28:06 -0400

    Update CV

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -10,28 +10,10 @@ import { ContactService } from '../contact.service';
 })
 export class ContactComponent implements OnInit {
 
-  formData: FormGroup;
-
   constructor(private builder: FormBuilder, private contact: ContactService) { }
 
   ngOnInit() {
-    this.formData = this.builder.group({
-      access_key: new FormControl('d78109c9-9464-42c4-81e2-ac493e1a0230', [Validators.required]),
-      name: new FormControl('', [Validators.required]),
-      email: new FormControl('', [Validators.compose([Validators.required, Validators.email])]),
-      message: new FormControl('', [Validators.required])
-    })
-  }
 
-  onSubmit(formData) {
-    console.log(formData)
-    this.contact.postMessage(formData).subscribe(response => {
-      console.log(response);
-    },
-    error => {
-      console.warn(error.responseText);
-      console.log(error);
-    })
   }
 
 }
